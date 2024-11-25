fed9db06bc6f0847ecce7ce0276d51c691b4c98a
gijsboddeus <gijs@afosto.io>
2024-11-25 17:03:37 +0100

    fix: added missing fragment declaration

--- src/queries/getAccountOrganisationUsersQuery.ts
+++ src/queries/getAccountOrganisationUsersQuery.ts
@@ -1,6 +1,9 @@
 import { gql } from '@afosto/graphql-client';
+import { CorePhoneNumberFragment } from '../fragments';
 
 export const getAccountOrganisationUsersQuery = gql`
+  ${CorePhoneNumberFragment}
+
   query GetAccountOrganisationUsers {
     account {
       shared_organisations {
